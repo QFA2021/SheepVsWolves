b7611132b9a51f535c6e1862c6ad505a0c89d812
Elias Heinemann <elias_heinemann@web.de>
2021-09-25 11:45:28 +0200

    added getImage and isValid

--- version1/pieces.py
+++ version1/pieces.py
@@ -1,25 +1,47 @@
 # -*- coding: utf-8 -*-
 import abc
-
+import os
+import game.py
 
 class Piece(abc.ABC):
-    
     # General values for all pieces:
-    # - getImage()
-    # - movement
+    # getImage() - check mate
     # abstract Movement
-    @abc.ABC.abstractmethod
+    @abc.abstracmethod
+    def getImage():
+        pass
+    
+    @abc.abstractmethod
     def isValid(xFrom, yFrom, xTo, yTo):
-        
+        pass
         
 class Sheep(Piece):
+    def getImage():
+        return os.path.join("/icons/sheep.png")
+    
+    def isValid(xFrom, yFrom, xTo, yTo):
+        free = game.isFree(xTo, yTo)
+        if free==False:
+            return False
+        #check sheep going down
+        down = (yTo >= yFrom)
+        #check step length
+        dist_square = (xTo-xFrom)^2 + (yTo-yFrom)^2
+        if down and (dist_square <= 2):
+            return True
+        return False
+    
+    
     # Create methods and variables for
     # - entangled
     # - superposition
     # - image depending on superposition / entanglement
     # - specific movement
-    pass
     
 class Wolf(Piece):
+    def getImage():
+        return os.path.join("/icons/wolf.png")
+    
+    
     # - specific Image
     # - specific movement
