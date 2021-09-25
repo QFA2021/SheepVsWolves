50d514d8555363514dc5be5dd9167e5005fab698
Martin Felber <martin.felber@tum.de>
2021-09-25 10:45:33 +0200

    added comments to classes

---
+++ version1/pieces.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+class Piece:
+    
+    # General values for all pieces:
+    # - getImage()
+    # - movement
+    
+    def __init__(x , y):
+        self.x = x
+        self.y = y
+        
+    # abstract Movement
+    @abstractmethod
+    def isValid(xFrom, yFrom, xTo, yTo):
+        
+        
+class Sheep(Piece): 
+    # Create methods and variables for
+    # - entangled
+    # - superposition
+    # - image depending on superposition / entanglement
+    # - specific movement
+    
+    
+class Wolf(Piece):
+    # - specific Image
+    # - specific movement
